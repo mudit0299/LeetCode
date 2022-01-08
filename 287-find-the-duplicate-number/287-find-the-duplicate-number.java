5064f8c9575f992ed04aef18e9c7be882fe1044a
Mudit gupta <47267051+mudit0299@users.noreply.github.com>
2022-01-08 15:39:23 +0530

    Time: 38 ms (10.43%), Space: 98.7 MB (5.02%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        // int total=(nums.length-1)*(nums.length)/2;
+        // int sum=0;
+        // for(int i=0;i<nums.length;i++) {
+        // 	sum=sum+nums[i];
+        // }
+        // return sum-total;
+        HashMap<Integer, Integer> n=new HashMap<Integer, Integer>();
+	for(int i=0;i<arr.length;i++) {
+		if(n.containsKey(arr[i])) {
+			return arr[i];
+			//n.put(arr[i],n.get(arr[i]+1));
+		}else {
+			n.put(arr[i],1);
+		}
+		
+	}
+	return 0;
+    }
+}
