e0a674a49e862df0c2805a784d0a724e12311115
Mudit gupta <47267051+mudit0299@users.noreply.github.com>
2022-02-07 22:51:39 +0530

    Time: 19 ms (5.66%), Space: 43.6 MB (5.97%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='A' && ch<='Z'){
+                ch=(char)(ch+32);
+                ans=ans+ch;
+                System.out.println(ans);
+            }else{
+                ans=ans+s.charAt(i);
+            }
+        }
+        return ans;
+    }
+}
