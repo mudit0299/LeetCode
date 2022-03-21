b7133925467678ee910fcee6d355180fcd8489c3
Mudit gupta <47267051+mudit0299@users.noreply.github.com>
2022-03-21 21:19:09 +0530

    Time: 14 ms (13.01%), Space: 53.9 MB (21.15%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+     StringBuffer sb = null;
+        for(String s:words){
+            sb = new StringBuffer(s);
+            if(sb.reverse().toString().equals(s))
+                return s;
+        }
+        return "";   
+    }
+}
